23e5204b7a1a4d3acf339807f4d8eb351d8e1c34
kainacorrea <alves_kaina@hotmail.com>
2024-03-08 18:06:42 -0300

    Inicio do projeto com index.html e Funcionario.java

---
+++ pi/src/main/java/com/senac/pi/data/FuncionarioEntity.java
@@ -0,0 +1,18 @@
+package com.senac.pi.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioEntity {
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private String observacores;
+}
