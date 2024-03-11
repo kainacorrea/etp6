a5e66236603c994ccb131388c3d9fbeb3f0544c4
kainacorrea <alves_kaina@hotmail.com>
2024-03-11 12:29:37 -0300

    Inclusão de informações do sql em application.properties; Declaração de FuncionarioEntity como uma tabela; Criação do CRUD em FuncionarioService

--- pi/src/main/java/com/senac/pi/data/FuncionarioEntity.java
+++ pi/src/main/java/com/senac/pi/data/FuncionarioEntity.java
@@ -1,5 +1,10 @@
 package com.senac.pi.data;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "funcionario")
 public class FuncionarioEntity {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String nome;
     private String cpf;
--- pi/src/main/java/com/senac/pi/service/FuncionarioService.java
+++ pi/src/main/java/com/senac/pi/service/FuncionarioService.java
@@ -3,6 +3,7 @@ package com.senac.pi.service;
 import com.senac.pi.data.FuncionarioEntity;
 import com.senac.pi.data.FuncionarioRepository;
 import com.senac.pi.exception.ResourceNotFoundException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,34 @@ public class FuncionarioService {
     public FuncionarioEntity getFuncionarioPorId(Integer funcId) {
         return funcionarioRepository.findById(funcId).orElseThrow(() -> new ResourceNotFoundException("Funcionário não encontrado"));
     }
+
+    public FuncionarioEntity cadastrarFuncionario(FuncionarioEntity f) {
+        f.setId(null);
+        funcionarioRepository.save(f);
+        return f;
+    }
     
+    public List<FuncionarioEntity> getTodosFuncionarios(){
+        return funcionarioRepository.findAll();
+    }
+    
+    public FuncionarioEntity atualizarFilme(Integer id, FuncionarioEntity funcionarioAtualizado) {
+        FuncionarioEntity f = getFuncionarioPorId(id);
+
+        f.setNome(funcionarioAtualizado.getNome());
+        f.setCpf(funcionarioAtualizado.getCpf());
+        f.setEmail(funcionarioAtualizado.getEmail());
+        f.setEndereco(funcionarioAtualizado.getEndereco());
+        f.setTelefone(funcionarioAtualizado.getTelefone());
+        f.setObservacoes(funcionarioAtualizado.getObservacoes());
+        
+        funcionarioRepository.save(f);
+        
+        return f;
+    }
+    
+    public boolean deletarFuncionario(Integer id){
+        funcionarioRepository.deleteById(getFuncionarioPorId(id).getId());
+        return true;
+    }
 }
