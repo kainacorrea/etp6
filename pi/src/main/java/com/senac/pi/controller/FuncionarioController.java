47085e872b0dfe28a3d4eec7f4a46511ce0821a1
kainacorrea <alves_kaina@hotmail.com>
2024-03-11 15:12:37 -0300

    Classe e folhas de estilo considerando cookies de preferência de modo de exibição da página

--- pi/src/main/java/com/senac/pi/controller/FuncionarioController.java
+++ pi/src/main/java/com/senac/pi/controller/FuncionarioController.java
@@ -1,11 +1,15 @@
 package com.senac.pi.controller;
 
 import com.senac.pi.data.FuncionarioEntity;
+import com.senac.pi.data.PreferenciaEntity;
 import com.senac.pi.service.FuncionarioService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FuncionarioController {
@@ -14,13 +18,15 @@ public class FuncionarioController {
     FuncionarioService funcionarioService;
             
     @GetMapping("/")
-    public String indexPagina (){
+    public String indexPagina (Model model, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
+        model.addAttribute("css", tema);
         return "index";
     }
     
     @GetMapping("/cadastro")
-    public String cadastroFuncionario(Model model){
+    public String cadastroFuncionario(Model model, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
         model.addAttribute("funcionario", new FuncionarioEntity());
+        model.addAttribute("css", tema);
         return "cadastrarView";
     }
     
@@ -32,23 +38,24 @@ public class FuncionarioController {
     }
     
     @GetMapping("/listagem")
-    public String listagemFuncionarios(Model model){
+    public String listagemFuncionarios(Model model, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
         model.addAttribute("funcionarios", funcionarioService.getTodosFuncionarios());
-        
+        model.addAttribute("css", tema);
         return "listarView";
     }
     
     @GetMapping("/detalhes")
-    public String mostrarDetalhes(@RequestParam String id, Model model){
+    public String mostrarDetalhes(@RequestParam String id, Model model, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
         model.addAttribute("funcionario", funcionarioService.getFuncionarioPorId(Integer.parseInt(id)));
-        
+        model.addAttribute("css", tema);
         return "mostrarView";
     }
     
     @GetMapping("/alterar")
-    public String alterarFuncionario(Model model, @RequestParam String id){
+    public String alterarFuncionario(Model model, @RequestParam String id, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
         model.addAttribute("funcionario", funcionarioService.getFuncionarioPorId(Integer.parseInt(id)));
-        
+        model.addAttribute("css", tema);
+         
         return "cadastrarView";
     }
     
@@ -66,9 +73,20 @@ public class FuncionarioController {
     }
     
     @PostMapping("/pesquisa")
-    public String pesquisarFuncionario(Model model, @RequestParam String cpf){
+    public String pesquisarFuncionario(Model model, @RequestParam String cpf, @CookieValue(name="temaUsuario", defaultValue="claro") String tema){
         model.addAttribute("funcionarios", funcionarioService.pesquisarFuncionarioPorCpf(cpf));
-        
+        model.addAttribute("css", tema);
         return "listarView";
+    }
+    
+    @PostMapping("/preferencia")
+    public ModelAndView salvarPreferencia(@ModelAttribute PreferenciaEntity p, HttpServletResponse response){
+        Cookie cookieTema = new Cookie("temaUsuario", p.getEstilo());
+        cookieTema.setDomain("localhost");
+        cookieTema.setHttpOnly(true);
+        cookieTema.setMaxAge(6400);
+        
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/");
     }
 }
---
+++ pi/src/main/java/com/senac/pi/data/PreferenciaEntity.java
@@ -0,0 +1,12 @@
+package com.senac.pi.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PreferenciaEntity {
+    private String estilo;
+}
